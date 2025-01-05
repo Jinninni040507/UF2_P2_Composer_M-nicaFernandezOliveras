ac7474c7be29a94a313637de6eb7e310d81fb543
Jinninni040507 <moni.oliveras05@gmail.com>
2025-01-05 16:59:46 +0100

    feat:  bootstrap installation & layout implementation

---
+++ src/View/layouts/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title ?? 'Mi Proyecto'; ?></title>
+    <link rel="stylesheet" href="../../vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
+</head>
+
+<body>
